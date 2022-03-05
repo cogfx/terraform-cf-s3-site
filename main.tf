4f1f08528f4edf3caab73dae83d0f5055f3bdca2
Gerry <gerald.king@gov.ab.ca>
2022-03-05 14:22:46 -0700

    main update

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+locals {
+  domain_name  = trimsuffix(var.domain_name, ".")
+  s3_origin_id = lower(random_id.oai.hex)
+  thistags  = {
+    terraform-uid     = lower(random_id.this.hex)
+    terraform-updated = timestamp()
+    environment       = var.environment
+  }
+}
+
 # Configure the default AWS Provider
 provider "aws" {
   region = "ca-central-1"
@@ -7,22 +17,25 @@ provider "aws" {
 
   default_tags {
     tags = {
-      terraform-uid = lower(random_id.this.hex)
+      terraform-uid     = lower(random_id.this.hex)
       terraform-updated = timestamp()
-      environment = var.environment
+      environment       = var.environment
     }
   }
 }
 
-# Configure additional AWS Provider
-# CloudFront expects ACM resources in us-east-1 region only
+# Configure additional AWS Provider - CloudFront expects ACM resources in us-east-1 region only
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
-}
 
-locals {
-  domain_name = trimsuffix(var.domain_name, ".")
+  default_tags {
+    tags = {
+      terraform-uid     = lower(random_id.this.hex)
+      terraform-updated = timestamp()
+      environment       = var.environment
+    }
+  }
 }
 
 resource "random_id" "this" {
@@ -39,7 +52,7 @@ module "s3_bucket_content" {
   version = "~> 2.0"
 
   bucket = "${lower(var.org)}-cc-${lower(var.environment)}-cloudfront-content-${lower(random_id.this.hex)}"
-  acl    = "private"
+  #acl    = "private"
 
   # organization SCP blocks public S3 buckets
   block_public_acls       = true
@@ -65,7 +78,7 @@ module "s3_bucket_logs" {
   version = "~> 2.0"
 
   bucket = "${lower(var.org)}-cc-${lower(var.environment)}-cloudfront-logs-${lower(random_id.this.hex)}"
-  acl    = "private"
+  #acl    = "private"
 
   # organization SCP blocks public S3 buckets
   block_public_acls       = true
@@ -85,41 +98,175 @@ module "s3_bucket_logs" {
   }
 }
 
-# ### Create TLS Certificate (AWS Certificate Manager)
-# resource "aws_acm_certificate" "this" {
-#   provider          = aws.us-east-1
-#   domain_name       = local.domain_name
-#   validation_method = "DNS"
+### Create TLS Certificate (AWS Certificate Manager)
+resource "aws_acm_certificate" "tls" {
+  provider          = aws.us-east-1
+  domain_name       = local.domain_name
+  validation_method = "DNS"
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# # ### Create CloudFront Distribution
-# # module "cdn" {
-# #   source = "terraform-aws-modules/cloudfront/aws"
-
-# #   aliases = ["${local.domain_name}"]
-
-# #   comment             = "${var.org} - S3 - (${var.environment})"
-# #   enabled             = true
-# #   is_ipv6_enabled     = true
-# #   price_class         = "PriceClass_100"
-# #   retain_on_delete    = false
-# #   wait_for_deployment = false
-
-# #   create_origin_access_identity = true
-# #   origin_access_identities = {
-# #     s3_bucket_one = "lower(${random_id.oai.hex})"
-# #   }
+output "example" {
+  value       = aws_acm_certificate.tls.domain_validation_options
+}
 
 
+### Create CloudFront Distribution
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI-${local.s3_origin_id}" # Sets OAI 'Name' value
+}
 
-# }
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = module.s3_bucket_content.s3_bucket_bucket_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "${local.domain_name}" # Sets distribtion 'Description' value
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = module.s3_bucket_logs.s3_bucket_bucket_domain_name
+    prefix          = "myprefix"
+  }
+
+  aliases = ["${local.domain_name}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  # tags = {
+  #   Environment = "production"
+  # }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.tls.arn
+    ssl_support_method  = "sni-only"
+  }
+}
 
 
 ### OAI - Data - IAM Policy - allow CF dist access S3 bucket
+data "aws_iam_policy_document" "s3_bucket_content" {
+  statement {
+    sid = "AllowCloudFrontListBucket"
+    actions = [
+      "s3:ListBucket"
+    ]
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "${aws_cloudfront_origin_access_identity.oai.iam_arn}"
+      ]
+    }
+    resources = [
+      module.s3_bucket_content.s3_bucket_arn
+    ]
+  }
+  statement {
+    sid = "AllowCloudFrontGetObject"
+    actions = [
+      "s3:GetObject"
+    ]
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "${aws_cloudfront_origin_access_identity.oai.iam_arn}"
+      ]
+    }
+    resources = [
+      format("%s/*", module.s3_bucket_content.s3_bucket_arn)
+    ]
+  }
+}
+
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = module.s3_bucket_content.s3_bucket_id
+  policy = data.aws_iam_policy_document.s3_bucket_content.json
+}
 
 ### OAI - Resource - S3 Bucket Policy - connect IAM policy to S3 bucket
 
